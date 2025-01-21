d6cfbcf4761aa2b572d32b299f641d64a173f88c
tharuAnish <namoanishtharu@gmail.com>
2025-01-21 19:02:41 +0530

    Setup Home Layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import BookList from "@/components/BookList"
+import BookOverview from "@/components/BookOverview"
+
+const Home = () => {
+  return (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  )
+}
+export default Home
